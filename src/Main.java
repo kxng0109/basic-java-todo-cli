7b59302b0e780bc8123ae7e6c4af99a86403165c
Joshua Ike <79171733+kxng0109@users.noreply.github.com>
2025-09-14 18:55:40 +0100

    Added persistence.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -8,6 +9,20 @@ public class Main {
 		ArrayList<String> tasks = new ArrayList<>();
 		boolean isRunning = true;
 
+		File file = new File("tasks.txt");
+
+		if(file.exists()){
+			try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))){
+				String line;
+				while ((line = bufferedReader.readLine()) != null){
+					line = line.trim();
+					if (!line.isEmpty()) tasks.add(line);
+				}
+			} catch (IOException e) {
+				System.out.println("An error occurred saving the task: " + e.getMessage());
+			}
+		}
+
 		while (isRunning){
 			printMenu();
 
@@ -17,17 +32,19 @@ public class Main {
 				System.out.println();
 
 				switch (userSelection){
-					case 1 -> addTask(scanner, tasks);
+					case 1 -> addTask(scanner, tasks, file);
 					case 2 -> listTasks(tasks);
-					case 3 -> markComplete(scanner, tasks);
-					case 4 -> removeTask(scanner, tasks);
+					case 3 -> markComplete(scanner, tasks, file);
+					case 4 -> removeTask(scanner, tasks, file);
 					case 5 -> {
+						saveAllTasks(tasks, file);
 						System.out.println("Bye!");
 						isRunning = false;
 					}
 					default -> System.out.println("Invalid input. Selection must be between 1 and 5!");
 				}
-			}catch (InputMismatchException e){
+			}
+			catch (InputMismatchException e){
 				scanner.nextLine();
 				System.out.println("Invalid input!");
 			}
@@ -45,7 +62,7 @@ public class Main {
 		System.out.print("Select an option (1-5): ");
 	}
 
-	static void addTask(Scanner scanner, ArrayList<String> tasks){
+	static void addTask(Scanner scanner, ArrayList<String> tasks, File file){
 		System.out.print("Enter the task you want to add to your To-Do list?: ");
 		String task = scanner.nextLine().trim();
 
@@ -55,7 +72,15 @@ public class Main {
 		}
 
 		tasks.add(task);
-		System.out.println("Task added!");
+
+		//Wanted to only save on exit, but why not save on every change 🤷🏾‍♂️
+		try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))){
+			bufferedWriter.write(task);
+			bufferedWriter.newLine();
+			System.out.println("Task added!");
+		} catch (IOException e){
+			System.out.println("An error occurred saving the task: " + e.getMessage());
+		}
 	}
 
 	static void listTasks(ArrayList<String> tasks){
@@ -70,45 +95,83 @@ public class Main {
 		}
 	}
 
-	static void markComplete(Scanner scanner, ArrayList<String> tasks){
+	static void markComplete(Scanner scanner, ArrayList<String> tasks, File file){
 		if(tasks.isEmpty()){
 			System.out.println("No tasks to complete.");
 			return;
 		}
 
-		System.out.printf("Enter the index of the task (1-%d) you want to mark as complete: ", tasks.size());
-		int i = scanner.nextInt();
-		scanner.nextLine();
+		System.out.printf("Enter the task number to mark as complete (1-%d): ", tasks.size());
 
-		int realIndex = i - 1;
+		int userIndex;
 
-		if(realIndex > tasks.size() || realIndex < 0){
+		try{
+			 userIndex = scanner.nextInt();
+			 scanner.nextLine();
+		}catch (InputMismatchException e){
+			scanner.nextLine();
+			System.out.println("Invalid input. Enter a number.");
+			return;
+		}
+
+		int realIndex = userIndex - 1;
+
+		if(realIndex >= tasks.size() || realIndex < 0){
 			System.out.printf("Invalid input! Enter the correct index (1-%d).\n", tasks.size());
 			return;
 		}
 
+		//Remove the task in memory
 		tasks.remove(realIndex);
 		System.out.println("Task marked as complete.");
+
+		//Then overwrite the file with the new tasks in memory
+		saveAllTasks(tasks, file);
 	}
 
-	static void removeTask(Scanner scanner, ArrayList<String> tasks){
+	static void removeTask(Scanner scanner, ArrayList<String> tasks, File file){
 		if(tasks.isEmpty()){
 			System.out.println("No tasks to remove.");
 			return;
 		}
 
 		System.out.printf("Enter the index of the task (1-%d) you want to remove: ", tasks.size());
-		int i = scanner.nextInt();
-		scanner.nextLine();
 
-		int realIndex = i - 1;
+		int userIndex;
 
-		if(realIndex > tasks.size() || realIndex < 0){
+		try{
+			userIndex = scanner.nextInt();
+			scanner.nextLine();
+		}catch (InputMismatchException e){
+			scanner.nextLine();
+			System.out.println("Invalid input. Enter a number.");
+			return;
+		}
+
+		int realIndex = userIndex - 1;
+
+		if(realIndex >= tasks.size() || realIndex < 0){
 			System.out.printf("Invalid input! Enter the correct index (1-%d).\n", tasks.size());
 			return;
 		}
 
+		//Remove the task in memory
 		tasks.remove(realIndex);
 		System.out.println("Task removed.");
+
+		//Then overwrite the file with the new tasks in memory
+		saveAllTasks(tasks, file);
+	}
+
+	//Save all tasks to the txt file
+	static void saveAllTasks(ArrayList<String> tasks, File file){
+		try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))){
+			for(String task : tasks){
+				bufferedWriter.write(task);
+				bufferedWriter.newLine();
+			}
+		}catch(IOException e){
+			System.out.println("An error occurred saving the task: " + e.getMessage());
+		}
 	}
 }
